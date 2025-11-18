0c328a8fa2cf90e775eb2cd6fb9e48456cc4fc04
Antoni Christodoulou <antoniagc@live.co.uk>
2025-11-18 22:58:29 +0000

    made cta buttons a bit smaller on mobile

--- app/components/ui/CtaLink.tsx
+++ app/components/ui/CtaLink.tsx
@@ -18,14 +18,14 @@ const CtaLink: React.FC<CtaLinkProps> = ({
   return (
     <Link
       href={href}
-      className={`group inline-flex items-center space-x-4 px-7 py-3 rounded-full font-semibold uppercase text-2xl tracking-[0.08em] hover:opacity-80 transition-opacity ${className}`}
+      className={`group inline-flex items-center space-x-3 px-5 py-2.5 md:space-x-4 md:px-7 md:py-3 rounded-full font-semibold uppercase text-lg md:text-2xl tracking-[0.08em] hover:opacity-80 transition-opacity ${className}`}
       style={{
         backgroundColor: "#8e8879",
         color: "#161616",
         fontFamily: '"din-2014", sans-serif',
       }}
     >
-      <span className="flex items-center justify-center w-7 h-7">
+      <span className="flex items-center justify-center w-6 h-6 md:w-7 md:h-7">
         <Image
           src="/Arrow.svg"
           alt="Arrow icon"
